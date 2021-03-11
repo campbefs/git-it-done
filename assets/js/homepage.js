fccad0d16a113adbf7507579c4afa12e42ae56b2
Christopher Campbell <campbefs@gmail.com>
2021-03-10 17:31:42 -0800

    fetch command for GitHub - dynamic

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,23 @@
-var getUserRepos = function() {
-  fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user) {
+  //fetch("https://api.github.com/users/octocat/repos");
+
+  // var response = fetch("https://api.github.com/users/octocat/repos");
+  // console.log(response);
+
+  // fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+  // console.log("inside", response);
+  // });
+
+  // console.log("outside");
+
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  fetch(apiUrl)
+    .then(function(response) {
+      response.json()
+        .then(function(data) {
+          console.log(data);
+          });
+      });
 };
 
-getUserRepos();
+getUserRepos("facebook");
