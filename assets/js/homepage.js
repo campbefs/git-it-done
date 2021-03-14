f9f0f2dc05642bc41b8a6ad75b5b8af8f40d0c9f
Christopher Campbell <campbefs@gmail.com>
2021-03-13 22:53:55 -0800

    added Search by Topic

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,6 @@
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var getUserRepos = function(user) {
   //fetch("https://api.github.com/users/octocat/repos");
@@ -110,3 +111,34 @@ var displayRepos = function(repos, searchTerm) {
 
 //getUserRepos("facebook");
 }
+
+var getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      // console.log(response);
+      response.json().then(function(data) {
+        displayRepos(data.items, language);
+      })
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  });
+};
+
+var buttonClickHandler = function(event) {
+  var language = event.target.getAttribute("data-language");
+  // console.log(language);
+
+  if (language) {
+    getFeaturedRepos(language);
+    
+    // clear old content
+    repoContainerEl.textContent= "";
+  }
+};
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
+
+
